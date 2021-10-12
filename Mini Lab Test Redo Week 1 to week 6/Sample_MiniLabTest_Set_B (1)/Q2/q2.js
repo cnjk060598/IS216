309d72ce1ef85c363fd09c6234b69e649279e251
Caleb Shawn <caleb.ng.2020@scis.smu.edu.sg>
2021-10-12 13:42:38 +0800

    push

---
+++ Mini Lab Test Redo Week 1 to week 6/Sample_MiniLabTest_Set_B (1)/Q2/q2.js
@@ -0,0 +1,16 @@
+// YOU MAY ADD MORE CODE BELOW
+// HINT: You might want to start with a function that is
+// invoked when any key is pressed in the calculator grid.
+
+
+
+// A sample function
+// Please name your function in a meaningful way
+// From the function name, it must be clear what business logic this function performs.
+// Make sure to console.log() and verify parameters, values, etc.
+function foo() {
+    console.log("=== [START] foo() ===")
+
+
+    console.log("=== [END] foo() ===")
+}
--- Mini Lab Test Redo Week 1 to week 6/Week 5/Week 5 - After Class/Resource/form/form.js
+++ Mini Lab Test Redo Week 1 to week 6/Week 5/Week 5 - After Class/Resource/form/form.js
@@ -1,7 +1,18 @@
 // Add Code here
 
-function getName() {
-    
-    // Add Code here
-  
+// step 1: get the input tag 
+
+let inputs = document.getElementsByTagName("input")
+let fname = inputs[0]
+let lname = inputs[1]
+
+fname.addEventListener("input", text)
+lname.addEventListener("input", text)
+
+function text(){
+    fname_value = fname.value
+    lname_value = lname.value
+
+    let text = `You have entered: ${fname_value} ${lname_value}`
+    document.getElementById("name").innerHTML = text
 }
